b64a1e0bc8166d8e6f235c9eaeadc6df8de91aff
Waabuffet <dominousas@gmail.com>
2023-12-15 23:17:43 +0100

    added rm to docker

--- run-docker.sh
+++ run-docker.sh
@@ -2,4 +2,4 @@
 
 current_dir=$(pwd)
 
-docker run -d --name p5-game -p 3000:3000 -v $current_dir:/app -w /app node:latest sh -c "npm install -g p5-server && p5 serve"
+docker run -d --rm --name p5-game -p 3000:3000 -v $current_dir:/app -w /app node:latest sh -c "npm install -g p5-server && p5 serve"
